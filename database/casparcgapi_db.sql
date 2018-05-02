6abd7f7b442a67b9d28d947ccd2eea7befcbcb5d
zak39 <fotia.baptiste@hotmail.com>
2018-05-03 00:47:28 +0200

    database: Remove table 'extension'

--- database/casparcgapi_db.sql
+++ database/casparcgapi_db.sql
@@ -12,18 +12,11 @@ create table if not exists type_media(
   nom_type varchar(40) not null
 );
 
-create table if not exists extension(
-  id_extension int auto_increment primary key,
-  nom_extension varchar(40) not null
-);
-
 create table if not exists medias(
   id_media int auto_increment primary key,
   id_type int not null,
-  id_extension int not null,
   nom_media varchar(40) not null,
   chemin varchar(100) not null,
   description varchar(200),
-  foreign key (id_type) references type_media(id_type),
-  foreign key (id_extension) references extension(id_extension)
+  foreign key (id_type) references type_media(id_type)
 );
