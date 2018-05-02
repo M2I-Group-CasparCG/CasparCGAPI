98e7eacfffebf573f07db49184db01fbcb77870e
zak39 <fotia.baptiste@hotmail.com>
2018-05-02 16:02:25 +0200

    database: Add tables -> comptes, type_media, extension, medias

---
+++ database/casparcgapi_db.sql
@@ -0,0 +1,29 @@
+-- Creation des tables
+create table compte(
+  id_compte int auto_increment primary key,
+  nom varchar(35) not null,
+  prenom varchar(35) not null,
+  pseudo varchar(35) not null,
+  password varchar(45) not null
+);
+
+create table type_media(
+  id_type int auto_increment primary key,
+  nom_type varchar(40) not null
+);
+
+create table extension(
+  id_extension int auto_increment primary key,
+  nom_extension varchar(40) not null
+);
+
+create table medias(
+  id_media int auto_increment primary key,
+  id_type int not null,
+  id_extension int not null,
+  nom_media varchar(40) not null,
+  chemin varchar(100) not null,
+  description varchar(200),
+  foreign key (id_type) references type_media(id_type),
+  foreign key (id_extension) references extension(id_extension)
+);
