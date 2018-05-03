6f79f1ffc2032505b2a6c64f7170ffc5ab1eb154
zak39 <fotia.baptiste@hotmail.com>
2018-05-03 21:55:06 +0200

    database: requests to search medias with their path

--- database/requests_select.sql
+++ database/requests_select.sql
@@ -31,3 +31,21 @@ select *
 from medias, type_media
 where medias.id_type = 3
 and medias.id_type = type_media.id_type;
+
+-- Afficher le chemin et le nom des medias qui sont des videos
+select chemin, nom_media
+from medias, type_media
+where medias.id_type = 1
+and medias.id_type = type_media.id_type;
+
+-- Afficher le chemin et le nom des medias qui sont des images
+select chemin, nom_media
+from medias, type_media
+where medias.id_type = 2
+and medias.id_type = type_media.id_type;
+
+-- Afficher le chemin et le nom des medias qui sont des sons
+select chemin, nom_media
+from medias, type_media
+where medias.id_type = 3
+and medias.id_type = type_media.id_type;
