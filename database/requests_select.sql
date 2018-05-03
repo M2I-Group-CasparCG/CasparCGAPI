1fbe4ef530542cbea75407854d0d63388a6feabe
zak39 <fotia.baptiste@hotmail.com>
2018-05-03 16:21:10 +0200

    database: Add requests select in requests_select.sql

---
+++ database/requests_select.sql
@@ -0,0 +1,33 @@
+-- selects
+
+-- Afficher tous les comptes
+select *
+from compte;
+
+-- Verifier si l'authentification est bonne
+
+-- Afficher tous les types de medias
+select *
+from type_media;
+
+-- Afficher tous les medias
+select *
+from medias;
+
+-- Afficher les medias qui sont des images
+select *
+from medias, type_media
+where medias.id_type = 2
+and medias.id_type = type_media.id_type;
+
+-- Afficher les medias qui sont des videos
+select *
+from medias, type_media
+where medias.id_type = 1
+and medias.id_type = type_media.id_type;
+
+-- Afficher les medias qui sont des sons
+select *
+from medias, type_media
+where medias.id_type = 3
+and medias.id_type = type_media.id_type;
